15373c4b303965bffabca2e37e3f76f77c7246b6
takijemai <73497985+takijemai@users.noreply.github.com>
2021-12-28 23:54:58 +0100

    Update main.tf

--- Mixto/modules/Google-Storage/main.tf
+++ Mixto/modules/Google-Storage/main.tf
@@ -1,12 +1,24 @@
-# *** YOUR CODE HERE ***
-# Crear un recurso google_storage_bucket para almacenar imágenes en Google Storage
-#
-# Crear en el bucket anteior dos recursos google_storage_bucket_object
-# (uno para cada imagen de la carpeta ./images)
-# Configurar la propiedad source desde la raíz del proyecto (./images/<imagen.jpg>)
-#
-# Definir una regla de acceso para objeto creado de forma que sea público 
-# Configurar google_storage_object_access_control así
-#   role   = "READER"
-#   entity = "allUsers"
-# **********************  
+
+resource "google_storage_bucket" ""image-store"" {
+  name        = "image-store-bucket"
+  location    = "EU"
+}
+
+
+resource "google_storage_bucket_object" "image" {
+  name   = "el_enigma_de_la_habitacion"
+  source = "./images/el_enigma_de_la_habitacion_622.jpg"
+  bucket = google_storage_bucket.image-store.name
+   role   = "READER"
+  entity = "allUsers"
+}
+
+
+resource "google_storage_bucket_object" "image2" {
+  name   = "una_historia_de_espana"
+  source = "./images/una_historia_de_espana.jpg"
+ bucket = google_storage_bucket.image-store.name
+   role   = "READER"
+  entity = "allUsers"
+}
+
