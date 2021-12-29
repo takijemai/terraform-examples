20a146878b045a341a7e3b911e21e663e40df6e8
takijemai <73497985+takijemai@users.noreply.github.com>
2021-12-29 23:23:02 +0100

    Update main.tf

--- Mixto/modules/Google-Storage/main.tf
+++ Mixto/modules/Google-Storage/main.tf
@@ -1,24 +1,27 @@
 
-resource "google_storage_bucket" ""image-store"" {
+resource "google_storage_bucket" "image-store" {
   name        = "image-store-bucket"
   location    = "EU"
+
 }
 
 
 resource "google_storage_bucket_object" "image" {
   name   = "el_enigma_de_la_habitacion"
   source = "./images/el_enigma_de_la_habitacion_622.jpg"
-  bucket = google_storage_bucket.image-store.name
-   role   = "READER"
-  entity = "allUsers"
+  bucket = "image-store"
 }
 
 
 resource "google_storage_bucket_object" "image2" {
   name   = "una_historia_de_espana"
   source = "./images/una_historia_de_espana.jpg"
- bucket = google_storage_bucket.image-store.name
-   role   = "READER"
+ bucket = "image-store"
+  
+}
+resource "google_storage_default_object_access_control" "public_rule" {
+  bucket = "image-store"
+  role   = "READER"
   entity = "allUsers"
 }
 
