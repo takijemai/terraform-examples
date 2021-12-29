928dd82ae3cfe1f86bdb10035e55230319c05bf0
takijemai <73497985+takijemai@users.noreply.github.com>
2021-12-30 00:47:20 +0100

    Update main.tf

--- Mixto/modules/OpenStack/main.tf
+++ Mixto/modules/OpenStack/main.tf
@@ -3,6 +3,35 @@
 # con el archivo install_mysql.sh
 # Asignarle una dirección IP flotante
 # **********************
+resource "openstack_compute_instance_v2" "mysql" {
+  name              = "mysql"
+  image_name        = "Ubuntu 16.04 LTS"
+  availability_zone = "nova"
+  flavor_name       = "medium"
+  key_pair          = var.openstack_keypair
+  security_groups   = ["default"]
+  network {
+    name = "jt800-net"
+  }
+
+  user_data = file("install_mysql.sh")
+}
+
+
+resource "openstack_networking_floatingip_v2" "mysql_ip" {
+  pool = "ext-net"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "mysql_ip" {
+  floating_ip = openstack_networking_floatingip_v2.mysql_ip.address
+  instance_id = openstack_compute_instance_v2.mysql.id
+}
+
+output MySQL_Floating_IP {
+  value      = openstack_networking_floatingip_v2.mysql_ip.address
+  depends_on = [openstack_networking_floatingip_v2.mysql_ip]
+}
+
 
 # Configura el archivo de plantilla para la API
 data "template_file" "setup-api-docker" {
@@ -20,23 +49,46 @@ resource "time_sleep" "wait_5_minutes" {
 
   create_duration = "5m"
 }
-
-resource "openstack_compute_instance_v2" "book_api" {
 # *** YOUR CODE HERE ***
 # Configuración de la instancia denominada book_api 
 # conectada a la red del proyecto e inicializada 
 # con el archivo de la plantilla ya inicializado setup-api-docker
 # **********************
+resource "openstack_compute_instance_v2" "book_api" {
+  name              = "book_api"
+  image_name        = "Ubuntu 16.04 LTS"
+  availability_zone = "nova"
+  flavor_name       = "medium"
+  key_pair          = var.openstack_keypair
+  security_groups   = ["default"]
+  network {
+    name = "jt800-net"
+  }
+
+  user_data = file("setup-api-docker.tpl")
+}
 
   user_data = data.template_file.setup-api-docker.rendered
 
   depends_on = [time_sleep.wait_5_minutes ]
 
 }
-
 # *** YOUR CODE HERE ***
 # Asignarle una dirección IP flotante
 # **********************
+resource "openstack_networking_floatingip_v2" "book_api" {
+  pool = "ext-net"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "book_api" {
+  floating_ip = openstack_networking_floatingip_v2.book_api.address
+  instance_id = openstack_compute_instance_v2.book_api.id
+}
+
+output book_api_Floating_IP {
+  value      = openstack_networking_floatingip_v2.book_api.address
+  depends_on = [openstack_networking_floatingip_v2.book_api]
+}
 
 # Configura el archivo de plantilla para la aplicación
 data "template_file" "setup-app-docker" {
@@ -54,6 +106,19 @@ resource "openstack_compute_instance_v2" "book_app" {
 # conectada a la red del proyecto e inicializada 
 # con el archivo de la aplicación ya inicializado setup-app-docker
 # **********************
+  resource "openstack_compute_instance_v2" "book_app" {
+  name              = "book_app"
+  image_name        = "Ubuntu 16.04 LTS"
+  availability_zone = "nova"
+  flavor_name       = "medium"
+  key_pair          = var.openstack_keypair
+  security_groups   = ["default"]
+  network {
+    name = "jt800-net"
+  }
+
+  user_data = file("setup-app-docker.tpl")
+}
 
   user_data = data.template_file.setup-app-docker.rendered
 }
@@ -61,6 +126,19 @@ resource "openstack_compute_instance_v2" "book_app" {
 # *** YOUR CODE HERE ***
 # Asignarle una dirección IP flotante
 # **********************
+resource "openstack_networking_floatingip_v2" "book_app" {
+  pool = "ext-net"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "book_app" {
+  floating_ip = openstack_networking_floatingip_v2.book_api.address
+  instance_id = openstack_compute_instance_v2.book_api.id
+}
+
+output book_app_Floating_IP {
+  value      = openstack_networking_floatingip_v2.book_app.address
+  depends_on = [openstack_networking_floatingip_v2.book_app]
+}
 
 # *** YOUR CODE HERE ***
 # Mostrar las direcciones IP generadas
