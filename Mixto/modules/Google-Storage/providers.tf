e1fcd3ed6f8ba8a03eb50c0a4e7b21340f23a0e2
takijemai <73497985+takijemai@users.noreply.github.com>
2021-12-30 17:42:16 +0100

    Update providers.tf

--- Mixto/modules/Google-Storage/providers.tf
+++ Mixto/modules/Google-Storage/providers.tf
@@ -8,9 +8,12 @@ required_providers {
 
 provider "google" {
      version = "3.5.0"
-credentials = file("../gcp-identity.json")
+credentials = file("../../gcp-identity.json")
 
   project = var.gcp-project
   region  = "us-central1"
-  zone    = "us-central1-a"
+  zone    = "us-central1-c"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
