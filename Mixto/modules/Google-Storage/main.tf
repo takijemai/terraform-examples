1db96312ed0f21882dc1be1a77549f04365979da
takijemai <73497985+takijemai@users.noreply.github.com>
2022-01-03 18:51:23 +0100

    Update main.tf

--- Mixto/modules/Google-Storage/main.tf
+++ Mixto/modules/Google-Storage/main.tf
@@ -1,27 +1,37 @@
 
-resource "google_storage_bucket" "image-store" {
-  name        = "image-store-bucket"
+
+resource "google_storage_bucket" "jt800-storage" {
+  name        = var.gcp_bucket_name
   location    = "EU"
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
 
 }
 
 
 resource "google_storage_bucket_object" "image" {
-  name   = "el_enigma_de_la_habitacion"
-  source = "./images/el_enigma_de_la_habitacion_622.jpg"
-  bucket = "image-store"
-}
-
-
-resource "google_storage_bucket_object" "image2" {
-  name   = "una_historia_de_espana"
-  source = "./images/una_historia_de_espana.jpg"
- bucket = "image-store"
+  name   = "image1"
+  source = "images/el_enigma_de_la_habitacion_622.jpg"
+  bucket = google_storage_bucket.jt800-storage.name
   
 }
-resource "google_storage_default_object_access_control" "public_rule" {
-  bucket = "image-store"
-  role   = "READER"
-  entity = "allUsers"
-}
 
+
+resource "google_storage_bucket_object" "fotos" {
+  name   = "image2"
+  source = "images/una_historia_de_espana.jpg"
+ bucket = google_storage_bucket.jt800-storage.name
+  
+}
+resource "google_storage_bucket_acl" "jt800-storage-acl" {
+  bucket = google_storage_bucket.jt800-storage.name
+  role_entity = [
+    "READER:AllUsers",
+  ]
+}
